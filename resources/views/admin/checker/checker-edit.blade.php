32340db6ff1e4ee1a972b4501e874e9c55872bc2
Rohit <rohit@gmail.com>
2024-11-30 15:28:33 +0530

    send notification

--- resources/views/admin/checker/checker-edit.blade.php
+++ resources/views/admin/checker/checker-edit.blade.php
@@ -329,6 +329,7 @@
                         {{-- <button type="submit" class="btn btn-primary">Update Checker</button>
                         <a href="{{ url('/admin/uploader/uploader-listing', $chitti->chittiId) }}" class="btn btn-primary">Send to Uploader</a> --}}
                         <button type="submit" class="btn btn-primary" name="action" value="update_checker">Update Checker</button>
+                        <button type="submit" class="btn btn-primary" name="action" value="send_to_maker">Send to Maker</button>
                         <button type="submit" class="btn btn-primary" name="action" value="send_to_uploader">Send to Uploader</button>
                     </div>
                 </form>
