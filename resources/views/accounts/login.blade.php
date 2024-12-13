566e62189b5968a031ed2963411d34e1386963f2
Rohit <rohit@gmail.com>
2024-12-13 17:21:42 +0530

    user credential

--- resources/views/accounts/login.blade.php
+++ resources/views/accounts/login.blade.php
@@ -11,6 +11,24 @@
                             <h5 class="mb-5 mt-2 text-dark">User Login</h5>
                         </div>
                         <hr>
+                        {{-- @if($errors->any())
+                        <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                            <ul class="mb-0">
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>
+                        @endif --}}
+                        @if (session('error'))
+                            <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                                {{ session('error') }}
+                                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                            </div>
+                        @endif
+
+
                         <form class="row g-3" action="{{ route('accounts.authenticate') }}" method="POST">
                             @csrf
                             <div class="col-12">
