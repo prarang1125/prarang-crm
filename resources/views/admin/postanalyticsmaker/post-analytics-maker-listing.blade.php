87cb9e772f36e3e44942757728f6422b90854863
ASG Kali <dev.vivek16@gmail.com>
2024-12-30 16:38:07 +0530

    new

--- resources/views/admin/postanalyticsmaker/post-analytics-maker-listing.blade.php
+++ resources/views/admin/postanalyticsmaker/post-analytics-maker-listing.blade.php
@@ -17,89 +17,90 @@
                     </ol>
                 </nav>
             </div>
-            <!--end breadcrumb-->
-            <div class="row">
-                <div class="col-xl-9 mx-auto w-100">
-                    <!-- Success Message -->
-                    @if (session('success'))
-                        <div class="alert alert-success mt-3">
-                            {{ session('success') }}
-                        </div>
-                    @endif
-                    <h6 class="mb-0 text-uppercase">Post Analytics Maker Listing</h6>
-                    <hr />
-                    <div class="card">
-                        <div class="card-body d-flex justify-content-end align-items-end">
-                            <!-- Search Form -->
-                            <form action="{{ url('admin/postanalyticsmaker/post-analytics-maker-listing') }}" method="GET"
-                                class="d-flex me-3">
-                                <input type="hidden" name="cityCode" value="{{ request()->query('cityCode') }}">
-                                <input type="text" name="search" class="form-control me-2" placeholder="Search by Title"
-                                    value="{{ request()->query('search') }}">
-                                <button type="submit" class="btn btn-secondary">Search</button>
-                            </form>
+        </div>
+        <!--end breadcrumb-->
+        <div class="row">
+            <div class="col-xl-9 mx-auto w-100">
+                <!-- Success Message -->
+                @if (session('success'))
+                    <div class="alert alert-success mt-3">
+                        {{ session('success') }}
+                    </div>
+                @endif
+                <h6 class="mb-0 text-uppercase">Post Analytics Maker Listing</h6>
+                <hr />
+                <div class="card">
+                    <div class="card-body d-flex justify-content-end align-items-end">
+                        <!-- Search Form -->
+                        <form action="{{ url('admin/postanalyticsmaker/post-analytics-maker-listing') }}" method="GET"
+                            class="d-flex me-3">
+                            <input type="hidden" name="cityCode" value="{{ request()->query('cityCode') }}">
+                            <input type="text" name="search" class="form-control me-2" placeholder="Search by Title"
+                                value="{{ request()->query('search') }}">
+                            <button type="submit" class="btn btn-secondary">Search</button>
+                        </form>
 
-                            @if (request()->has('search'))
-                                <a class="btn btn-primary me-1"
-                                    href="{{ url()->current() }}?cityCode={{ request()->query('cityCode') }}">
-                                    <i class="bx bx-refresh"></i> Clear
-                                </a>
-                            @endif
-                        </div>
+                        @if (request()->has('search'))
+                            <a class="btn btn-primary me-1"
+                                href="{{ url()->current() }}?cityCode={{ request()->query('cityCode') }}">
+                                <i class="bx bx-refresh"></i> Clear
+                            </a>
+                        @endif
+                    </div>
 
-                        <div class="card-body">
-                            <table class="table mb-0 table-hover">
-                                <thead class="thead-light">
+                    <div class="card-body">
+                        <table class="table mb-0 table-hover">
+                            <thead class="thead-light">
+                                <tr>
+                                    {{-- <th scope="col" class="">#</th> --}}
+                                    <th scope="col" class="">Sr.</th>
+                                    <th scope="col" class="">Title</th>
+                                    <th scope="col" class="">Upload Date</th>
+                                    <th scope="col" class="">Days</th>
+                                    <th scope="col" class="">Area</th>
+                                    {{-- <th scope="col" class="">Ad</th> --}}
+                                    <th scope="col" class="">Viewership</th>
+                                    {{-- <th scope="col" class="">Status</th> --}}
+                                    {{-- <th scope="col" class="">Sent Time</th> --}}
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @php
+                                    $index = ($chittis->currentPage() - 1) * $chittis->perPage() + 1;
+                                @endphp
+                                @foreach ($chittis as $chitti)
                                     <tr>
-                                        {{-- <th scope="col" class="">#</th> --}}
-                                        <th scope="col" class="">Sr.</th>
-                                        <th scope="col" class="">Title</th>
-                                        <th scope="col" class="">Upload Date</th>
-                                        <th scope="col" class="">Days</th>
-                                        <th scope="col" class="">Area</th>
-                                        {{-- <th scope="col" class="">Ad</th> --}}
-                                        <th scope="col" class="">Viewership</th>
-                                        {{-- <th scope="col" class="">Status</th> --}}
-                                        {{-- <th scope="col" class="">Sent Time</th> --}}
+                                        {{-- <th scope="row" class="text-center">{{ $index }}</th> --}}
+                                        <td class="">{{ $index }}</td>
+                                        <td class="">
+                                            <a href="{{ route('admin.post-analytics-maker-create', ['id' => $chitti->chittiId, 'city' => $chitti->cityCode ?? 'N/A']) }}"
+                                                class="text-primary">
+                                                {{ $chitti->Title }}
+                                            </a>
+                                        </td>
+                                        <td class="">
+                                            {{ \Carbon\Carbon::parse($chitti->createDate)->format('d-M-Y') }}
+                                        </td>
+                                        <td class="">
+                                            {{ (int) \Carbon\Carbon::parse($chitti->createDate)->diffInDays(now()) }}
+                                        </td>
+                                        <td class="">{{ $chitti->citynameInEnglish ?? 'N/A' }} </td>
+
+                                        <td class="">{{ $chitti->totalViewerCount }}</td>
+                                        {{-- <td class="">{{ $chitti->finalStatus }}</td> --}}
+
                                     </tr>
-                                </thead>
-                                <tbody>
-                                    @php
-                                        $index = ($chittis->currentPage() - 1) * $chittis->perPage() + 1;
-                                    @endphp
-                                    @foreach ($chittis as $chitti)
-                                        <tr>
-                                            {{-- <th scope="row" class="text-center">{{ $index }}</th> --}}
-                                            <td class="">{{ $index }}</td>
-                                            <td class="">
-                                                <a href="{{ route('admin.post-analytics-maker-create', ['id' => $chitti->chittiId, 'city' => $chitti->cityCode ?? 'N/A']) }}"
-                                                    class="text-primary">
-                                                    {{ $chitti->Title }}
-                                                </a>
-                                            </td>
-                                            <td class="">
-                                                {{ \Carbon\Carbon::parse($chitti->createDate)->format('d-M-Y') }}
-                                            </td>
-                                            <td class="">
-                                                {{ (int) \Carbon\Carbon::parse($chitti->createDate)->diffInDays(now()) }}
-                                            </td>
-                                            <td class="">{{ $chitti->citynameInEnglish ?? 'N/A' }} </td>
-
-                                            <td class="">{{ $chitti->totalViewerCount }}</td>
-                                            {{-- <td class="">{{ $chitti->finalStatus }}</td> --}}
-
-                                        </tr>
-                                        @php $index++;  @endphp
-                                    @endforeach
-                                </tbody>
-                            </table>
-                            <div class="d-flex justify-content-end mt-4">
-                                {{ $chittis->links('pagination::bootstrap-5') }}
-                            </div>
+                                    @php $index++;  @endphp
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="d-flex justify-content-end mt-4">
+                            {{ $chittis->links('pagination::bootstrap-5') }}
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        <!--end page wrapper -->
-    @endsection
+    </div>
+    <!--end page wrapper -->
+@endsection
