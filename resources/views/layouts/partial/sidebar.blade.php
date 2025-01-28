9751c07ada6c975f959842167e39ec2431f2d2b1
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-01-28 17:51:16 +0530

    NEwVisit

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -38,6 +38,10 @@
                 <li> <a href="{{ url('/admin/postanalytics/post-analytics-listing') }}"><i
                             class="bx bx-right-arrow-alt"></i>Post Analytics</a>
                 </li>
+                <li> <a href="{{ route('visitor') }}"><i
+                            class="bx bx-right-arrow-alt"></i>visitors</a>
+                </li>
+
             </ul>
         </li>
         <li>
