a3a8ea9889339e8f4b6f23db6e5cbed40ecb5cd5
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-05-08 09:25:17 +0530

    CRM Enhance

--- app/Livewire/Post/WriterReport.php
+++ app/Livewire/Post/WriterReport.php
@@ -10,8 +10,6 @@ use Livewire\Component;
 
 class WriterReport extends Component
 {
-
-
     public $writers = [];
     public $mUserName = [];
     public $writerId = null;
@@ -24,30 +22,29 @@ class WriterReport extends Component
 
     public function mount()
     {
-        $this->allWriters=Muser::whereIn('roleId', [1,2])->get();
+        $this->allWriters = Muser::whereIn('roleId', [1, 2])->get();
         $this->startDate = Carbon::now()->startOfMonth()->format('d-m-Y h:i A');
         $this->endDate = Carbon::now()->endOfMonth()->format('d-m-Y h:i A');
-
     }
 
-
-    public function submit(){
+    public function submit()
+    {
 
         $this->startDate = Carbon::createFromDate($this->selectedYear, $this->selectedMonth, 1)->format('d-m-Y h:i A');
         $this->endDate = Carbon::createFromDate($this->selectedYear, $this->selectedMonth, 1)->endOfMonth()->format('d-m-Y h:i A');
         $this->writers = Muser::where('userId', $this->writerId)
-        ->join('chitti', 'chitti.makerId', '=', 'muser.userId')
-        ->whereBetween(
-            DB::raw("STR_TO_DATE(chitti.dateOfApprove, '%d-%m-%Y %h:%i %p')"),
-            [
-                Carbon::parse($this->startDate)->format('Y-m-d H:i:s'),
-                Carbon::parse($this->endDate)->format('Y-m-d H:i:s'),
-            ]
-        )
-        ->select('muser.userId', DB::raw('COUNT(chitti.chittiId) as count'))
-        ->groupBy('muser.userId')
-        ->pluck('count', 'userId')
-        ->toArray();
+            ->join('chitti', 'chitti.makerId', '=', 'muser.userId')
+            ->whereBetween(
+                DB::raw("STR_TO_DATE(chitti.dateOfApprove, '%d-%m-%Y %h:%i %p')"),
+                [
+                    Carbon::parse($this->startDate)->format('Y-m-d H:i:s'),
+                    Carbon::parse($this->endDate)->format('Y-m-d H:i:s'),
+                ]
+            )
+            ->select('muser.userId', DB::raw('COUNT(chitti.chittiId) as count'))
+            ->groupBy('muser.userId')
+            ->pluck('count', 'userId')
+            ->toArray();
         $this->posts = Chitti::where('makerId', $this->writerId)
             ->whereBetween(
                 DB::raw("STR_TO_DATE(chitti.dateOfApprove, '%d-%m-%Y %h:%i %p')"),
@@ -62,15 +59,10 @@ class WriterReport extends Component
             ->get()
             ->toArray();
 
-
-
-
         $this->mUserName = Muser::select('userId', DB::raw("concat(firstName, ' ', lastName) as name"))
             ->pluck('name', 'userId')
             ->toArray();
     }
-
-
     public function render()
     {
         return view('livewire.post.writer-report')->layout('components.layouts.admin.base');
