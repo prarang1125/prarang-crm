12f9105dda3fe317dc5d07a66d7a679cc6eb6275
ASG_Kali(AVR) <vivek.k@prarang.in>
2025-07-05 09:56:50 +0530

    old

--- app/Livewire/Marketing/HitBox.php
+++ app/Livewire/Marketing/HitBox.php
@@ -11,29 +11,29 @@ use Illuminate\Support\Str;
 class HitBox extends Component
 {
     public $cities = [], $cityIds = [], $city = [];
-    public $groups, $group,$userGroup=[2,4];
-    public  $user,$customUsers;
+    public $groups, $group, $userGroup = [2, 4];
+    public  $user, $customUsers;
     public $message;
-    public $templates, $template, $templateId=1;
-    public $contacts=[];
-    public $channel=[1],$channels=[];
+    public $templates, $template, $templateId = 1;
+    public $contacts = [];
+    public $channel = [1], $channels = [];
     public $showPost = false;
     public $content = '';
-    public $posts = [],$postId;
+    public $posts = [], $postId;
 
     public function mount()
     {
         $this->cities = DB::connection('yp')->table('cities')->get();
-        $this->channels=['1'=>'WhatsApp','2'=>'Email','3'=>'SMS'];
+        $this->channels = ['1' => 'WhatsApp', '2' => 'Email', '3' => 'SMS'];
         $this->templates = $this->getTemplates();
         $this->setTemplates();
         $this->posts = $this->getDailyPost();
-        $dd=Chitti::select('chittiId','Title','SubTitle')->orderBydesc('created_at')->limit(10)->get();
-
+        $dd = Chitti::select('chittiId', 'Title', 'SubTitle')->orderBydesc('created_at')->limit(10)->get();
     }
 
     public function cityUpdate()
-    {        $this->city= DB::connection('yp')->table('cities')->whereIn('id',$this->cityIds)->get();
+    {
+        $this->city = DB::connection('yp')->table('cities')->whereIn('id', $this->cityIds)->get();
         $this->setTemplates();
     }
 
@@ -60,7 +60,7 @@ class HitBox extends Component
     {
 
         $this->contacts = $this->getContacts();
-        SendWhatsAppMessage::dispatch('917619876249','hello_world')->onQueue('whatsapp');
+        SendWhatsAppMessage::dispatch('917619876249', 'hello_world')->onQueue('whatsapp');
 
         // foreach ($this->contacts as $user) {
         //     $phone="91".ltrim($user->phone, '0');
@@ -74,47 +74,44 @@ class HitBox extends Component
 
     private function getContacts()
     {
-        $contacts=[];
+        $contacts = [];
         if ($this->city) {
-            $contacts = DB::connection('yp')->table('users')->whereIn('city_id', collect($this->city)->pluck('id'))->whereIn('role',$this->userGroup)->get()->toArray();
+            $contacts = DB::connection('yp')->table('users')->whereIn('city_id', collect($this->city)->pluck('id'))->whereIn('role', $this->userGroup)->get()->toArray();
         }
         return $contacts;
-
     }
 
     public function getTemplates()
     {
 
         return [
-            ['id' => 1,'name'=>'Daily Post','format'=>"नमस्ते {user},पढ़े आज का लेख़:"],
-            ['id' => 2,'name'=>'Template 2','format'=>'Dear {name},\n Need To Update It Welcome to our site-2'],
-            ['id' => 3,'name'=>'Template 3','format'=>'Dear {name},\n Need To Update It Welcome to our site-3'],
-            ['id' => 4,'name'=>'Template 3','format'=>'Dear {name},\n Need To Update It Welcome to our site-3'],
+            ['id' => 1, 'name' => 'Daily Post', 'format' => "नमस्ते {user},पढ़े आज का लेख़:"],
+            ['id' => 2, 'name' => 'Template 2', 'format' => 'Dear {name},\n Need To Update It Welcome to our site-2'],
+            ['id' => 3, 'name' => 'Template 3', 'format' => 'Dear {name},\n Need To Update It Welcome to our site-3'],
+            ['id' => 4, 'name' => 'Template 3', 'format' => 'Dear {name},\n Need To Update It Welcome to our site-3'],
         ];
-
     }
     public function setTemplates()
     {
-        if($this->templateId==1){
+        if ($this->templateId == 1) {
             $this->showPost = true;
-        }else{
+        } else {
             $this->showPost = false;
         }
         $this->template = collect($this->templates)->where('id', $this->templateId)->first()['format'];
-        if($this->city){
+        if ($this->city) {
             $this->template = str_replace('{city_name}', collect($this->city)->pluck('name')->implode(','), $this->template);
         }
         $this->content = $this->template;
-        if($this->postId && $this->showPost){
-            $post=Chitti::find($this->postId);
-            $this->content.= "\n\n https://www.prarang.in/rampur-chitti/".Str::slug($post->SubTitle);
+        if ($this->postId && $this->showPost) {
+            $post = Chitti::find($this->postId);
+            $this->content .= "\n\n https://www.prarang.in/rampur-chitti/" . Str::slug($post->SubTitle);
             // $this->template = str_replace('{name}', $this->user, $this->template);
         }
     }
 
     private function getDailyPost()
     {
-        return Chitti::select('chittiId','Title','SubTitle')->orderBydesc('created_at')->limit(10)->get();
+        return Chitti::select('chittiId', 'Title', 'SubTitle')->orderBydesc('created_at')->limit(10)->get();
     }
-
 }
