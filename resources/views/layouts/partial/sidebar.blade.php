39cde198ec8ef6c419e59441f30143285700459b
Vivek Yadav <dev.vivek16@gmail.com>
2026-09-09 17:04:22 +0530

    old

--- resources/views/layouts/partial/sidebar.blade.php
+++ resources/views/layouts/partial/sidebar.blade.php
@@ -5,7 +5,7 @@
     <div>
         <h4 class="logo-text">PRARANG</h4>
     </div>
-    <div class="toggle-icon ms-auto">
+    <div class="ms-auto toggle-icon">
         {{-- Optional toggle icon --}}
     </div>
 </div>
@@ -19,37 +19,42 @@
             <div class="menu-title">Dashboard</div>
         </a>
         <ul>
-            <li> <a href="{{ url('/admin/dashboard') }}"><i class="bx bx-right-arrow-alt"></i>Home</a>
+            <li> <a href="{{ url('/admin/dashboard') }}"><i class="bx-right-arrow-alt bx"></i>Home</a>
             </li>
-            <li> <a href="{{ url('/admin/maker/maker-listing') }}"><i class="bx bx-right-arrow-alt"></i>Maker</a>
+            <li> <a href="{{ url('/admin/maker/maker-listing') }}"><i class="bx-right-arrow-alt bx"></i>Maker</a>
             </li>
             <li> <a href="{{ url('/admin/checker/checker-listing') }}"><i
-                        class="bx bx-right-arrow-alt"></i>Checker</a>
+                        class="bx-right-arrow-alt bx"></i>Checker</a>
             </li>
             <li> <a href="{{ url('/admin/uploader/uploader-listing') }}"><i
-                        class="bx bx-right-arrow-alt"></i>Uploader</a>
+                        class="bx-right-arrow-alt bx"></i>Uploader</a>
             </li>
 
             <!-- Sudhanshu -->
             <li>
                 <a href="{{ url('ads/ad-listing') }}">
-                    <i class="bx bx-right-arrow-alt"></i>Ads Uploads</a>
+                    <i class="bx-right-arrow-alt bx"></i>Ads Uploads</a>
             </li>
-            <!-- Sudhanshu -->
+              <!-- Sudhanshu -->
+             <li>
+                <a href="{{ url('admin/portal_ads') }}">
+                    <i class="bx-right-arrow-alt bx"></i>Portal Ads</a>
+            </li>
+          
 
 
 
 
             <li> <a href="{{ url('/admin/postanalyticsmaker/post-analytics-maker-city-listing') }}"><i
-                        class="bx bx-right-arrow-alt"></i>Post Analytics Maker</a>
+                        class="bx-right-arrow-alt bx"></i>Post Analytics Maker</a>
             </li>
             <li> <a href="{{ url('/admin/postanalyticschecker/post-analytics-checker-city-listing') }}"><i
-                        class="bx bx-right-arrow-alt"></i>Post Analytics Checker</a>
+                        class="bx-right-arrow-alt bx"></i>Post Analytics Checker</a>
             </li>
             <li> <a href="{{ url('/admin/postanalytics/post-analytics-listing') }}"><i
-                        class="bx bx-right-arrow-alt"></i>Post Analytics</a>
+                        class="bx-right-arrow-alt bx"></i>Post Analytics</a>
             </li>
-            <li> <a href="{{route('content.post-listing')}}"><i class="bx bx-right-arrow-alt"></i>Posts Filter</a>
+            <li> <a href="{{route('content.post-listing')}}"><i class="bx-right-arrow-alt bx"></i>Posts Filter</a>
             </li>
             <li> <a href="/subscribers"><i class="bx bx-user"></i>Subscribers</a>
             </li>
@@ -65,45 +70,45 @@
             <div class="menu-title">Admin</div>
         </a>
         <ul>
-            <li> <a href="{{ url('/admin/user-profile') }}"><i class="bx bx-right-arrow-alt"></i>User Profile</a>
+            <li> <a href="{{ url('/admin/user-profile') }}"><i class="bx-right-arrow-alt bx"></i>User Profile</a>
             </li>
-            <li> <a href="{{ url('/admin/user-listing') }}"><i class="bx bx-right-arrow-alt"></i>User</a>
-            <li> <a href="{{ route('our-team.index') }}"><i class="bx bx-right-arrow-alt"></i>Our Teams</a>
+            <li> <a href="{{ url('/admin/user-listing') }}"><i class="bx-right-arrow-alt bx"></i>User</a>
+            <li> <a href="{{ route('our-team.index') }}"><i class="bx-right-arrow-alt bx"></i>Our Teams</a>
             </li>
     </li>
-    <li> <a href="{{ url('/admin/role/role-listing') }}"><i class="bx bx-right-arrow-alt"></i>Role</a>
+    <li> <a href="{{ url('/admin/role/role-listing') }}"><i class="bx-right-arrow-alt bx"></i>Role</a>
     </li>
     <li> <a href="{{ url('/admin/languagescript/languagescript-listing') }}"><i
-                class="bx bx-right-arrow-alt"></i>Language Script</a>
+                class="bx-right-arrow-alt bx"></i>Language Script</a>
     </li>
-    <li> <a href="{{ url('/admin/country/country-listing') }}"><i class="bx bx-right-arrow-alt"></i>Country</a>
+    <li> <a href="{{ url('/admin/country/country-listing') }}"><i class="bx-right-arrow-alt bx"></i>Country</a>
     </li>
-    <li> <a href="{{ url('/admin/livecity/live-city-listing') }}"><i class="bx bx-right-arrow-alt"></i>Live
+    <li> <a href="{{ url('/admin/livecity/live-city-listing') }}"><i class="bx-right-arrow-alt bx"></i>Live
             City</a>
     </li>
-    <li> <a href="{{ url('/admin/scities/scities-listing') }}"><i class="bx bx-right-arrow-alt"></i>City</a>
+    <li> <a href="{{ url('/admin/scities/scities-listing') }}"><i class="bx-right-arrow-alt bx"></i>City</a>
     </li>
-    <li> <a href="{{ url('/admin/region/region-listing') }}"><i class="bx bx-right-arrow-alt"></i>Region</a>
+    <li> <a href="{{ url('/admin/region/region-listing') }}"><i class="bx-right-arrow-alt bx"></i>Region</a>
         {{-- Portal: Vivek --}}
-    <li> <a href="{{ route('portal.index') }}"><i class="bx bx-right-arrow-alt"></i>Portals</a>
+    <li> <a href="{{ route('portal.index') }}"><i class="bx-right-arrow-alt bx"></i>Portals</a>
     </li>
     {{-- End Portal:Vivek --}}
     </li>
-    <li> <a href="{{ url('/admin/tagcategory/tag-category-listing') }}"><i class="bx bx-right-arrow-alt"></i>Tag
+    <li> <a href="{{ url('/admin/tagcategory/tag-category-listing') }}"><i class="bx-right-arrow-alt bx"></i>Tag
             Category</a>
     </li>
-    <li> <a href="{{ url('/admin/tag/tag-listing') }}"><i class="bx bx-right-arrow-alt"></i>Tag</a>
+    <li> <a href="{{ url('/admin/tag/tag-listing') }}"><i class="bx-right-arrow-alt bx"></i>Tag</a>
     </li>
-    <li> <a href="{{ url('/admin/usercountry/user-country-listing') }}"><i class="bx bx-right-arrow-alt"></i>User
+    <li> <a href="{{ url('/admin/usercountry/user-country-listing') }}"><i class="bx-right-arrow-alt bx"></i>User
             Country</a>
     </li>
-    <li> <a href="{{ url('/admin/usercity/user-city-listing') }}"><i class="bx bx-right-arrow-alt"></i>User
+    <li> <a href="{{ url('/admin/usercity/user-city-listing') }}"><i class="bx-right-arrow-alt bx"></i>User
             City</a>
     </li>
-    <li> <a href="{{ url('/admin/post/post-listing') }}"><i class="bx bx-right-arrow-alt"></i>Post</a>
+    <li> <a href="{{ url('/admin/post/post-listing') }}"><i class="bx-right-arrow-alt bx"></i>Post</a>
     </li>
     <li> <a href="{{ url('/admin/deleted-post/deleted-post-listing') }}"><i
-                class="bx bx-right-arrow-alt"></i>Deleted Post</a>
+                class="bx-right-arrow-alt bx"></i>Deleted Post</a>
     </li>
 </ul>
 </li>
@@ -118,23 +123,23 @@
         @if (Auth::user()->roleId == 2 || Auth::user()->roleId == 13 || Auth::user()->roleId == 14)
         <li>
             <a href="{{ url('/accounts/maker/dashboard') }}">
-                <i class="bx bx-right-arrow-alt"></i> Maker
+                <i class="bx-right-arrow-alt bx"></i> Maker
             </a>
         </li>
         @endif
         @if (Auth::user()->roleId == 3 || Auth::user()->roleId == 14)
-        <li> <a href="{{ url('/accounts/checker/dashboard') }}"><i class="bx bx-right-arrow-alt"></i>Checker</a>
+        <li> <a href="{{ url('/accounts/checker/dashboard') }}"><i class="bx-right-arrow-alt bx"></i>Checker</a>
             @endif
             @if (Auth::user()->roleId == 4 || Auth::user()->roleId == 13 || Auth::user()->roleId == 14)
-        <li> <a href="{{ url('/accounts/uploader/dashboard') }}"><i class="bx bx-right-arrow-alt"></i>Uploader</a></li>
+        <li> <a href="{{ url('/accounts/uploader/dashboard') }}"><i class="bx-right-arrow-alt bx"></i>Uploader</a></li>
         @endif
         @if (Auth::user()->roleId == 6 )
         <li> <a href="{{ url('/accounts/postanalyticsmaker/acc-post-analytics-maker-city-listing') }}"><i
-                    class="bx bx-right-arrow-alt"></i>Analytics Maker</a>
+                    class="bx-right-arrow-alt bx"></i>Analytics Maker</a>
             @endif
             @if (Auth::user()->roleId == 7 )
         <li> <a href="{{ url('/accounts/postanalyticschecker/acc-post-analytics-checker-city-listing') }}"><i
-                    class="bx bx-right-arrow-alt"></i>Analytics Checker</a>
+                    class="bx-right-arrow-alt bx"></i>Analytics Checker</a>
             @endif
     </ul>
 </li>
